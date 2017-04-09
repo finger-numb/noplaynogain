c203e13667561ce54496249aec6421d33d039fa6
TT95 <teotoplak95@gmail.com>
2017-04-09 04:10:44 +0200

    Made google api autocomplete for landing page

--- public/javascripts/googlePlaces.js
+++ public/javascripts/googlePlaces.js
@@ -1,27 +1,17 @@
-/**
- * Created by teo on 11/29/16.
- */
-// This example adds a search box to a map, using the Google Place Autocomplete
-// feature. People can enter geographical searches. The search box will return a
-// pick list containing a mix of places and predicted search terms.
 
-// This example requires the Places library. Include the libraries=places
-// parameter when you first load the API. For example:
-// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
-
-var BTplaces;
 
 function initAutocomplete() {
-        // var map = new google.maps.Map(document.getElementById('map'), {
-        //     center: {lat: 38, lng: 260},
-        //     zoom: 5,
-        //     mapTypeId: 'roadmap'
-        // });
 
-    // Create the search box and link it to the UI element.
-    var input = document.getElementById('pac-input');
+    // inputs.forEach(function(input) {
+    doGoogleMaps('pac-input');
+    doGoogleMaps('pac-input2');
+
+}
+
+function doGoogleMaps(className) {
+    var input = document.getElementById(className);
     var searchBox = new google.maps.places.SearchBox(input);
-        // map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+    // map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
     // Bias the SearchBox results towards current map's viewport.
     // map.addListener('bounds_changed', function() {
@@ -33,34 +23,14 @@ function initAutocomplete() {
     // more details for that place.
     searchBox.addListener('places_changed', function() {
         var places = searchBox.getPlaces();
-        // window.location.replace("http://localhost:9000");
-
         var finalPlace;
+        var num = 0;
         places.forEach(function(place) {
-                finalPlace = place;
-                num++;
-            });
-        initMap(finalPlace)
-
-        // $.ajax({
-        //     type: "GET",
-        //     url: "/register"
-        // });
-        // if (places.length == 0) {
-        //     return;
-        // }
-        // var finalPlace;
-        // var num = 0;
-        // places.forEach(function(place) {
-        //     finalPlace = place;
-        //     num++;
-        // });
-        // // map.fitBounds(bounds);
-        // if(num>1) {
-        //     alert("You have selected " + num + "places. Select just one!")
-        // } else {
-        //     window.location.href = "http://localhost:9000/watplace/" +  finalPlace.place_id;
-        // };
-});
+            finalPlace = place;
+            num++;
+        });
+        $(function () {
+            $('#' + className).val(finalPlace.name);
+        });
+    });
 }
-
